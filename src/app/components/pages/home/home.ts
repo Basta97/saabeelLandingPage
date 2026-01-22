9f7e885f8a90acf71768060e0b5f88b05e103549
AhmedBasta <ahmed.b.01.12.97@gmail.com>
2026-01-22 13:28:09 +0200

    to continue

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../../sections/hero/hero';
 import { Features } from "../../sections/features/features";
 import { Futuref } from "../../sections/futuref/futuref";
+import { Package } from "../../sections/package/package";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Features, Futuref],
+  imports: [Hero, Features, Futuref, Package],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
